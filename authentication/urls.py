bec87a7981c606b30c3248dc77302a2caf9ecf84
Jiajun Dai <jdai10@mail.ccsf.edu>
2024-10-26 22:40:10 -0700

    minor fix

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import signup_view, login_view, exchange_code
+from .views import exchange_code, app_signup, app_login
 
 urlpatterns = [
-    path('signup/', signup_view, name='signup'),
-    path('login/', login_view, name='login'),
+    path('signup/', app_signup, name='signup'),
+    path('login/', app_login, name='login'),
     path('exchange-code/', exchange_code, name='exchange_code'),
 ]
